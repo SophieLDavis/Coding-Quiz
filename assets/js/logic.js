9f3f372f0ddb93e8b4b9e48e989768fa1bffda9d
Sophie <sophielouisedavis@live.com>
2023-01-23 19:18:00 +0000

    clear choice container and append next set of choices

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,76 +5,33 @@ var questions= document.querySelector('#questions');
 var title= document.querySelector('#question-title')
 var startScreen= document.querySelector('#start-screen')
 
-//timer function counts down from 100 
-//var startTimer= function(){
-/* var counter=100;
-var runTimer= setInterval(function() {
-quizTimer.textContent = counter 
-    counter--
-    if (counter<=0) {
-    //endGame()
-}
-}, 1000)
-} */
-
-//function to run/ start game when startQuiz button is pressed 
-//var startQuiz= function(){
-//startTimer()
-//startScreen.setAttribute('class', 'hide')
-//questions.setAttribute('class', 'visible')
-//questionTitle()
-//createChoiceList()
-//}
-
-
-//function to add question to page heading 
-
-
-/* var questionTitle= function(){
-for(i=1; i<title.length; i++); {
-title.textContent= quizQuestions[i];
-} 
-} */
-
-//function to add question choices to page 
+//Add question one choices to page 
 var createChoiceList= function(){
 var choiceList= document.createElement('ul')
 choicesContainer.appendChild(choiceList)
-var questionChoices=[]
-for(i=0; i<4; i++) {
-questionChoices= answers[i]// answers[0], answers[1], answers[2]
+for(i=0; i<4; i++) { 
 var choice= document.createElement('button')
 choiceList.appendChild(choice)
-choice.textContent= questionChoices
-}
-}
-
+choice.textContent= answers[0][i]
+}}
+//show the next question 
 currentQuestion=0
 var nextQuestion= function() {
-console.log(currentQuestion++)
+currentQuestion++
 if (currentQuestion< quizQuestions.length) {
-   // for(i=1; i<title.length; i++); {
-        title.textContent= quizQuestions[currentQuestion];
-        } 
-       // }
-   //(quizQuestions[currentQuestion]);//  */
-//
-//} else {
-//    endgame()
-//}
-}
-    
-// hide 
-// 
+title.textContent= quizQuestions[currentQuestion];
+//clear choice container and show next set of choices
+choicesContainer.innerHTML=''
+var choiceList= document.createElement('ul')
+choicesContainer.appendChild(choiceList)
+for(i=0; i<4; i++) { 
+var choice= document.createElement('button')
+choiceList.appendChild(choice)
+choice.textContent= answers[currentQuestion][i]
+} 
+}}
 
-
-//setAttribute(name, value)
-/* startButton.addEventListener('click', startQuiz);{
-    startScreen.setAttribute('class', 'hide')
-    questions.setAttribute('class', 'visible')
-    } */
-
-
+//first event listener
 startButton.addEventListener('click', function() {
 startScreen.setAttribute('class', 'hide')
 questions.setAttribute('class', 'visible')
@@ -94,17 +51,14 @@ if (counter<=0) {
 }, 1000)
 })
 
-        
-
-
-//event listener, when a choice button is clicked, answer is stored and the next question page comes up. 
-//variable.addEventListener(event, function)
-
+//second event listener, when a choice button is clicked 
 choicesContainer.addEventListener('click',function() {
 nextQuestion()
-console.log("hi")
-
-//currentQuestion =0;
-//populateQuestion(quizQuestions[currentQuestion]) // show question[0]
-
 })
+
+//Another click event listener for choices
+//    Check answer
+//        if correct, add 1 to score, call nextQuestion()
+//        if wrong, remove 10 seconds from the interval, call nextQuestion
+
+//when button is clicked, answer[0][i] is replaced with [0][i+3]
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -13,56 +13,13 @@ var quizQuestions=[
 ["Question Seven: In JavaScript, what element is used to store multiple values in a single variable?"],
 ["Question Eight: In JavaScript, what element is used to store multiple values in a single variable?"]
 ]
-
-
-/* var answersQOne = { 
-    A:"Hyper Trainer Marking Language", B:"Hyper Text Marketing Language", C:"Hyper Text Markup Language", D:"Hyper Text Markup Leveler", Answer:"C"}
-var answerQTwo = {
-    A:"Cleaner", B:"Spacing", C:"Break", D:"Padding", Answer:"D"}
-var answerQThree = {
-    A:"Tables",  B:"Objects", C:"Code", D:"Boxes", Answer:"D"}
-var answerQFour = {
-    A:"PHP", B:"XML", C:"CSS", D:"HTML", Answer:"C"}
-var answerQFive = {
-    A:"Gallery", B:"Table", C:"Boxes", D:"Shadow Box", Answer:"A"}
-var answerQSix = {
-    A:"Dimension", B:"Box", C:"Size", D:"Block", Answer:"B"}
-var answerQSeven = { 
-    A:"Arrays", B:"Strings", C:"Variables", D:"Functions", Answer:"A"}
-var AnswerQEight = {
-    A:"In the <footer> section.", B:"In the <title> section.", C:"In the <meta> section.", D:"In the <body> and <head> sections.", Answer:"D"}
-
-    var answers=[
-    //var answersQOne = 
-        ["Hyper Trainer Marking Language","Hyper Text Marketing Language","Hyper Text Markup Language","Hyper Text Markup Leveler"],{Answer:"C"},
-    //var answerQTwo = 
-        ["Cleaner","Spacing","Break", "Padding"],{Answer:"D"},
-    //var answerQThree = 
-        ["Tables","Objects","Code","Boxes"],{Answer:"D"},
-    //var answerQFour = 
-        ["PHP", "XML", "CSS","HTML"],{Answer:"C"},
-    //var answerQFive = 
-        ["Gallery", "Table", "Boxes","Shadow Box"],{Answer:"A"},
-    //var answerQSix = 
-        ["Dimension", "Box", "Size", "Block"], {Answer:"B"},
-    //var answerQSeven = 
-        ["Arrays", "Strings", "Variables", "Functions"], {Answer:"A"},
-   // var AnswerQEight = {
-        ["In the <footer> section.", "In the <title> section.", "In the <meta> section.", "In the <body> and <head> sections."], {Answer:"D"}]
-    
- */
-
-    var answers=
-       ["Hyper Trainer Marking Language","Hyper Text Marketing Language","Hyper Text Markup Language","Hyper Text Markup Leveler",
-      "Cleaner","Spacing","Break","Padding",
-        "Tables","Objects","Code","Boxes",
-         "PHP", "XML", "CSS","HTML",
-        "Gallery", "Table", "Boxes","Shadow Box",
-        "Dimension", "Box", "Size","Block",
-    "Arrays", "Strings", "Variables", "Functions",
-        "In the <footer> section.","In the <title> section.","In the <meta> section.","In the <body> and <head> sections."]
-     
-    
-
-//console.log(answers)
-
+ 
+var answers=[
+["Hyper Trainer Marking Language","Hyper Text Marketing Language","Hyper Text Markup Language","Hyper Text Markup Leveler"],
+["Cleaner","Spacing","Break","Padding"],
+["Tables","Objects","Code","Boxes"],
+["PHP", "XML", "CSS","HTML"],
+["Gallery", "Table", "Boxes","Shadow Box"],
+["Dimension", "Box", "Size","Block"],
+["Arrays", "Strings", "Variables", "Functions"],
+["In the <footer> section.","In the <title> section.","In the <meta> section.","In the <body> and <head> sections."]]
